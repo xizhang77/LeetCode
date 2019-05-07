dfbacba75f38d2d1a70c6065941950aa47eeba76
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-07 14:50:25 -0700

    Update 39_Combination_Sum.py
    Add time & space complexity

--- Backtracking/39_Combination_Sum.py
+++ Backtracking/39_Combination_Sum.py
@@ -27,6 +27,20 @@ A solution set is:
   [3,5]
 ]
 '''
+# 参考：https://github.com/Deadbeef-ECE/Interview/blob/master/Leetcode/BackTracking/039_Combination_Sum.java
+
+# Time: O( k * 2^n')
+# 此题可以转换成 Combination Sum II, 如何做呢, 举个栗子:
+# int[] arr = {2, 3, 4, 5, 6}, target = 10; 我们知道此题中,每个元素可以重复用, 
+# 其实, 如果把 arr 变成 {2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6}, 然
+# 后每个元素只能用一次, 就变成了Combination Sum II的要求了. 
+# 我们再看新数组, 元素多了很多, 多了多少? 
+# 那就是数组中所有小于等于target的元素E增加了ceil(target/E)个, 然后就可以用
+# Combination Sum II的方法分析复杂度了. 这里n'是新arr的大小
+
+# Space: O(n)
+# n: length of candidates
+# worst case: candidates = [1,...] and target = n
 
 class Solution(object):
     def dfs(self, nums, target, path, ans):
