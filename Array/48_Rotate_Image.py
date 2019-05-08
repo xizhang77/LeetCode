0be2f28f19af47d2328e0010bf58c2c1394d9f6f
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-07 17:10:25 -0700

    Create 48_Rotate_Image.py

---
+++ Array/48_Rotate_Image.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+'''
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+'''
+
+# Time: O(n); Space: O(1)
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len( matrix )
+        
+        for i in range( n - 1 ):
+            for j in range( i + 1, n ):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
+            
