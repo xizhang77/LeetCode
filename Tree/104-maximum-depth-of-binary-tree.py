3aa2dda2c1f38619a32999068954c5fcd9477bfb
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-09 15:18:19 -0700

    Create 104-maximum-depth-of-binary-tree.py

---
+++ Tree/104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+'''
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# [Solution 1: Recursive]
+class Solution1(object):
+    def dfs(self, root, level ):
+        if not root:
+            return level
+        return max(self.dfs( root.left, level + 1), self.dfs( root.right, level + 1))
+    
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return self.dfs( root, 0 )
+
+
+# [Solution 2: Iterative]
+class Solution2(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        level = 0
+        
+        if not root:
+            return level
+        
+        current = [ root ]
+        
+        while current:
+            level += 1
+            nextLevel = []
+            for node in current:
+                if node.left:
+                    nextLevel.append( node.left )
+                if node.right:
+                    nextLevel.append( node.right )
+            current = nextLevel
+        
+        return level
