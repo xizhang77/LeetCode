34093d619c19cff297e692b68091dea6c90db23e
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-22 21:53:58 -0700

    Update 221-maximal-square.py

--- DynamicProgramming/221-maximal-square.py
+++ DynamicProgramming/221-maximal-square.py
@@ -32,28 +32,18 @@ class Solution(object):
         
         m, n = len(matrix), len(matrix[0])
         
-        dp = [ [ 0 for _ in range(n)] for _ in range(m) ]
+        dp = [ [0]*n for _ in range(m) ]
         
-        ans = 0
         
-        if matrix[0][0] == '1':
-            dp[0][0] = 1 
-            ans = 1 
+        for i in range(m):
+            dp[i][0] = int( matrix[i][0] )
         
-        for i in range(1, m):
-            if matrix[i][0] == '1':
-                ans = 1
-                dp[i][0] = 1
-        
-        for j in range(1, n):
-            if matrix[0][j] == '1':
-                ans = 1
-                dp[0][j] = 1
+        for j in range(n):
+            dp[0][j] = int( matrix[0][j] )
         
         for i in range(1, m):
             for j in range(1, n):
                 if matrix[i][j] == '1':
                     dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
-                    ans = max( ans, dp[i][j]**2)
         
-        return ans
+        return max( max(row) for row in dp ) ** 2
