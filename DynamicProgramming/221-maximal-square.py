8855a8f61901d4174310baa29572454f0bd603f3
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-22 21:49:01 -0700

    Create 221-maximal-square.py

---
+++ DynamicProgramming/221-maximal-square.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+'''
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+Example:
+
+Input: 
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+Output: 4
+'''
+
+'''
+这道题期初想仿照LeetCode #85 maximal rectangle的做法，但是失败了。我觉得应该可以再抢救一下… 
+'''
+
+# Solution 1: DP
+# Time and Space: O(m*n)
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+        
+        m, n = len(matrix), len(matrix[0])
+        
+        dp = [ [ 0 for _ in range(n)] for _ in range(m) ]
+        
+        ans = 0
+        
+        if matrix[0][0] == '1':
+            dp[0][0] = 1 
+            ans = 1 
+        
+        for i in range(1, m):
+            if matrix[i][0] == '1':
+                ans = 1
+                dp[i][0] = 1
+        
+        for j in range(1, n):
+            if matrix[0][j] == '1':
+                ans = 1
+                dp[0][j] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
+                    ans = max( ans, dp[i][j]**2)
+        
+        return ans
