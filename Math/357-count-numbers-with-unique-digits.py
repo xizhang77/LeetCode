a2b71cfc29ca742015e7d6edc0df4d419f7d666b
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-24 19:37:09 -0700

    Create 357-count-numbers-with-unique-digits.py

---
+++ Math/357-count-numbers-with-unique-digits.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+'''
+Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+
+Example:
+
+Input: 2
+Output: 91 
+Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, 
+             excluding 11,22,33,44,55,66,77,88,99
+'''
+
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 1
+        
+        ans = 10
+        factor = 9
+        
+        for i in range(n-1):
+            factor = factor * (9-i)
+            ans += factor
+        
+        return ans
