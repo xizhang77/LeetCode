de480a5936310d53d177b1bad313cb79551aac1d
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-29 16:28:27 -0700

    Create 87-scramble-string.py

---
+++ DynamicProgramming/87-scramble-string.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+'''
+Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".
+
+    rgtae
+   /    \
+  rg    tae
+ / \    /  \
+r   g  ta  e
+       / \
+      t   a
+We say that "rgtae" is a scrambled string of "great".
+
+Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+
+Example 1:
+
+Input: s1 = "great", s2 = "rgeat"
+Output: true
+Example 2:
+
+Input: s1 = "abcde", s2 = "caebd"
+Output: false
+'''
+
+
+# Solution 1: DP (Using a Dict to memorize)
+# Refer: https://leetcode.com/problems/scramble-string/discuss/29452/Python-dp-solutions-(with-and-without-memorization).
+class Solution(object):
+    def __init__(self):
+        self.dic = {}
+    def isScramble(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if (s1, s2) in self.dic:
+            return self.dic[ (s1, s2) ]
+        
+        if len(s1) != len(s2) or sorted(s1) != sorted(s2):
+            self.dic[ (s1, s2) ] = False
+            return False
+        
+        if s1 == s2 or s1 == s2:
+            self.dic[ (s1, s2) ] = True
+            return True
+        
+        
+        for i in range( 1, len(s1) ):
+            if (self.isScramble( s1[:i], s2[:i] ) and self.isScramble( s1[i:], s2[i:]) ) or \
+            ( self.isScramble( s1[:i], s2[-i:]) and self.isScramble( s1[i:], s2[:-i] ) ):
+                self.dic[ (s1, s2) ] = True
+                return True
+        
+        self.dic[ (s1, s2) ] = False
+        return False
+
+
+# Solution 2: Recursive
+# Refer: https://blog.csdn.net/fuxuemingzhu/article/details/84950326
+class Solution(object):
+    def isScramble(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) != len(s2) or sorted(s1) != sorted(s2):
+            return False
+        
+        if s1 == s2:
+            return True
+        
+        for i in range( 1, len(s1) ):
+            if (self.isScramble( s1[:i], s2[:i] ) and self.isScramble( s1[i:], s2[i:]) ) or \
+            ( self.isScramble( s1[:i], s2[-i:]) and self.isScramble( s1[i:], s2[:-i] ) ):
+                return True
+        
+        return False
