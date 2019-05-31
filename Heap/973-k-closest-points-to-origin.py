3aa24f73b718af2acef49c5c45c439b965029edd
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-30 23:10:12 -0700

    Create 973-k-closest-points-to-origin.py

---
+++ Heap/973-k-closest-points-to-origin.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+'''
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+ 
+
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation: 
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+Example 2:
+
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+(The answer [[-2,4],[3,3]] would also be accepted.)
+ 
+
+Note:
+
+1 <= K <= points.length <= 10000
+-10000 < points[i][0] < 10000
+-10000 < points[i][1] < 10000
+'''
+
+# Time: O(n) for create the heap and O(klogn) to extract
+# Space: O(k)
+import heapq
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        
+        for point in points:
+            dist = point[0]**2 + point[1]**2
+            heapq.heappush( ans, [ - dist, point ] )
+            
+            if len( ans ) > K:
+                heapq.heappop( ans )
+        
+        return [ value[1] for value in ans ]
