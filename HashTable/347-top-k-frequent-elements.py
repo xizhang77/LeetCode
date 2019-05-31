2b1f77e72b50932302a9ed932af503c0fdbcc622
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-05-31 16:25:37 -0700

    Create 347-top-k-frequent-elements.py

---
+++ HashTable/347-top-k-frequent-elements.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+'''
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+'''
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+        
+        hashMap = {}
+        
+        for num in nums:
+            if num not in hashMap:
+                hashMap[ num ] = 1
+            else:
+                hashMap[ num ] += 1
+
+        count = sorted(hashMap.items(), key=lambda x: x[1], reverse = True)
+        return [ x[0] for x in count ][:k]
