54e2763201d8162c1b00da8ac84dc0ca83f19219
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-06-03 17:27:01 -0700

    Create 187-repeated-dna-sequences.py

---
+++ HashTable/187-repeated-dna-sequences.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+'''
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+Example:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+
+Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+'''
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        ans = []
+        
+        hashMap = {}
+        
+        for i in range( len(s) - 10 + 1 ):
+            if s[i:i+10] not in hashMap:
+                hashMap[ s[i:i+10] ] = i
+            else:
+                if s[i:i+10] not in ans:
+                    ans.append( s[i:i+10] )
+
+        
+        return ans
