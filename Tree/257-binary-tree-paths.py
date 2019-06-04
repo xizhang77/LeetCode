c189610e49965c747fe455e6a607056db8cf8420
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-06-04 12:35:04 -0700

    Create 257-binary-tree-paths.py

---
+++ Tree/257-binary-tree-paths.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+'''
+Given a binary tree, return all root-to-leaf paths.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+Output: ["1->2->5", "1->3"]
+
+Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def dfs(self, root, path, ans):
+        if not root:
+            return
+        
+        if root.left:
+            self.dfs( root.left, path + [ str(root.val) ], ans )
+        if root.right:
+            self.dfs( root.right, path + [ str(root.val) ], ans )
+        
+        if not root.left and not root.right:
+            ans.append( path + [ str(root.val) ] )
+            return
+        
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        ans = []
+        
+        self.dfs( root, [], ans )
+        
+        return [ "->".join(val) for val in ans ]
