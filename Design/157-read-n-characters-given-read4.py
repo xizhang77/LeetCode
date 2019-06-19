a2faec5c33f29f0b12d82ebfed64e1b8cd59c339
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-06-19 14:15:05 -0700

    Create 157-read-n-characters-given-read4.py

---
+++ Design/157-read-n-characters-given-read4.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+'''
+https://leetcode.com/problems/read-n-characters-given-read4/
+'''
+
+"""
+The read4 API is already defined for you.
+
+    @param buf, a list of characters
+    @return an integer
+    def read4(buf):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        ans = 0
+        while ans <= n:
+            temp = [' '] * 4
+            length = read4(temp)
+            buf[ans: min(n, ans+length)] = temp
+            ans += min(n-ans, length)
+            if length < 4:
+                return ans
+            
+        return ans
