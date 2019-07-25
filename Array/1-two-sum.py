165737e8d635e2c2057134d955b8df7dd4ec072d
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-07-24 18:37:11 -0700

    Update 1-two-sum

--- Array/1-two-sum.py
+++ Array/1-two-sum.py
@@ -37,6 +37,7 @@ class Solution(object):
                 else:
                     return sorted([idx for idx in range( len(nums)) if nums[idx] == sortNum[i]])
 
+
 # Solution 2: Hash Table (Time: O(n); Space: O(n))
 class Solution(object):
     def twoSum(self, nums, target):
@@ -58,4 +59,29 @@ class Solution(object):
                 return hashMap[ nums[i] ] + hashMap[ target - nums[i] ]
             elif target - nums[i] == nums[i] and len( hashMap[ nums[i] ] ) == 2:
                 return hashMap[ nums[i] ]
-                
+
+
+# Solution 3: Binary Search (Time: O(nlogn))
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        sort = sorted( nums, reverse = False )
+        
+        for k, num in enumerate( sort ):
+            i, j = k+1, len(nums) - 1
+            while i <= j:
+                mid = i + (j-i)/2
+                if sort[mid] > target - num:
+                    j = mid - 1
+                elif sort[mid] < target - num:
+                    i = mid + 1
+                else:
+                    if num == sort[mid]:
+                        return [idx for idx in range(len(nums)) if nums[idx] == num ]
+                    else:
+                        return sorted([nums.index(num), nums.index( sort[mid])])
