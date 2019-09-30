b4ec89da635d0da0c90d2de875df87649ed26106
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-09-30 16:15:43 -0700

    Update 244-shortest-word-distance-ii.py

--- Design/244-shortest-word-distance-ii.py
+++ Design/244-shortest-word-distance-ii.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 '''
-Design a class which receives a list of words in the constructor, and implements a method that takes two words word1 and word2 and return the shortest distance between these two words in the list. Your method will be called repeatedly many times with different parameters. 
+Design a class which receives a list of words in the constructor, 
+and implements a method that takes two words word1 and word2 and return 
+the shortest distance between these two words in the list. 
+Your method will be called repeatedly many times with different parameters. 
 
 Example:
 Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
@@ -11,9 +14,52 @@ Output: 3
 Input: word1 = "makes", word2 = "coding"
 Output: 1
 Note:
-You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+You may assume that word1 does not equal to word2, 
+and word1 and word2 are both in the list.
 '''
 
+# Time: O(n)+O(k), where k is the longest lengh of idx
+# Space: O(n)
+class WordDistance(object):
+
+    def __init__(self, words):
+        """
+        :type words: List[str]
+        """
+        self.hashmap = {}
+        for i, word in enumerate( words ):
+            if word in self.hashmap:
+                self.hashmap[ word ] += [i]
+            else:
+                self.hashmap[ word ] = [ i ]
+                
+
+    def shortest(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        idx1 = self.hashmap[ word1 ]
+        idx2 = self.hashmap[ word2 ]
+        
+        ans = float('inf')
+        
+        p, q = 0, 0
+        
+        while p < len(idx1) and q < len(idx2):
+            ans = min( ans, abs(idx1[p] - idx2[q]) )
+            if idx1[p] < idx2[q]:
+                p += 1
+            else:
+                q += 1
+        
+        return ans
+
+
+# Solution2: Space complexity is unpredicable...
+# Misunderstand the question... called repeatedly many times with "different" parameters.
+# So the usage of dictionary is enough.
 class WordDistance(object):
 
     def __init__(self, words):
@@ -53,6 +99,9 @@ class WordDistance(object):
         else:
             self.check[ temp ] = self.distance( word1, word2 )
             return self.check[ temp ]
+
+
+
 
 
 # Your WordDistance object will be instantiated and called as such:
