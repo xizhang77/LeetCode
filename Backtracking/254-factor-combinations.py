8c4a098cd85eb8290d8379a18d4d8a499e2a7154
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-10-01 17:49:55 -0700

    Update 254-factor-combinations.py

--- Backtracking/254-factor-combinations.py
+++ Backtracking/254-factor-combinations.py
@@ -4,9 +4,32 @@
 https://leetcode.com/problems/factor-combinations/
 '''
 
-
+# Solution 1: backtracking
 from math import sqrt
-class Solution(object):
+class Solution1(object):
+    def dfs(self, n, path ):
+        if n < 2:
+            return
+        
+        for i in range(2, int(sqrt(n))+1):
+            if path and i < path[-1]:
+                continue
+            if n%i == 0:
+                self.ans.append( path + [i, n/i] )
+                self.dfs( n/i, path + [i] )
+        
+    def getFactors(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        self.ans = []
+        self.dfs( n, [] )
+        
+        return self.ans
+
+# Solution 2 
+class Solution2(object):
     def solver(self, start, n):
         if n < 2:
             return []
