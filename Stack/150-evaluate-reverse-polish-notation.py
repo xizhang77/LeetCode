b427c2f52a51f678fddf886bd7855961142b1c2b
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-10-01 19:00:58 -0700

    Update 150-evaluate-reverse-polish-notation.py

--- Stack/150-evaluate-reverse-polish-notation.py
+++ Stack/150-evaluate-reverse-polish-notation.py
@@ -33,3 +33,41 @@ class Solution(object):
                 stack.append( int(val) )
                 
         return stack[0]
+
+
+# Solution 2
+# Time: O(n); Space: O(1) [no extra space needed]
+# However, during practice, it's much slower than solution 1 because the size changes over time
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        i = 0
+        while i < len(tokens):
+            if tokens[i].isdigit() or tokens[i][1:].isdigit():
+                i += 1
+                continue
+            
+            i -= 2
+            num1 = int( tokens.pop(i) )
+            num2 = int( tokens.pop(i) )
+            
+            if tokens[i] == '+':
+                tokens[i] = str( num1 + num2 )
+            elif tokens[i] == '-':
+                tokens[i] = str( num1 - num2 )
+            elif tokens[i] == '*':
+                tokens[i] = str( num1*num2 )
+            else:
+                sign = 1
+                if num1 * num2 < 0:
+                    sign = - 1
+                num1, num2 = abs(num1), abs(num2)
+                tokens[i] = str( sign * (num1/num2) )
+            
+            
+            # print tokens
+        
+        return int( tokens[0] )
